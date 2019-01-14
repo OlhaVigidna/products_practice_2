8ae0b3308c4aacf3b696228b4ad2f9c93c45bd35
OlhaVigidna <olya.vigidna@gmail.com>
2019-01-14 21:32:51 +0200

    commit!

--- src/main/java/com/example/practice_3/config/WebConfig.java
+++ src/main/java/com/example/practice_3/config/WebConfig.java
@@ -20,15 +20,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public JavaMailSenderImpl javaMailSender(){
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost(env.getProperty("email.host"));
-        mailSender.setPort(Integer.parseInt(env.getProperty("email.port")));
-        mailSender.setUsername(env.getProperty("email.username"));
-        mailSender.setPassword(env.getProperty("email.password"));
+        mailSender.setHost(env.getProperty("mail.host"));
+        mailSender.setPort(Integer.parseInt(env.getProperty("mail.port")));
+        mailSender.setUsername(env.getProperty("mail.username"));
+        mailSender.setPassword(env.getProperty("mail.password"));
         Properties properties = mailSender.getJavaMailProperties();
-        properties.put(env.getProperty("email.protocol"), env.getProperty("email.protocol.value"));
-        properties.put(env.getProperty("email.auth"), env.getProperty("email.auth.value"));
-        properties.put(env.getProperty("email.starttls"), env.getProperty("email.starttls.value"));
-        properties.put(env.getProperty("email.mail.debug"), env.getProperty("email.mail.debug.value"));
+        properties.put(env.getProperty("mail.protocol"), env.getProperty("mail.protocol.value"));
+        properties.put(env.getProperty("mail.auth"), env.getProperty("mail.auth.value"));
+        properties.put(env.getProperty("mail.starttls"), env.getProperty("mail.starttls.value"));
+        properties.put(env.getProperty("mail.mail.debug"), env.getProperty("mail.mail.debug.value"));
         return mailSender;
 
     }
--- src/main/java/com/example/practice_3/services/impl/MailServiceImpl.java
+++ src/main/java/com/example/practice_3/services/impl/MailServiceImpl.java
@@ -5,7 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 @Service
 @PropertySource("classpath:application.properties")
@@ -14,15 +19,23 @@ public class MailServiceImpl implements MailServise{
     @Autowired
     private JavaMailSender javaMailSender;
 
-    @Override
-    public void send(String email, String message) {
-
-    }
 
     @Autowired
     private Environment env;
 
-    public void send(String email){
+    public void send(String email, String message){
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = null;
+        try {
+            helper = new MimeMessageHelper(mimeMessage, true);
+            mimeMessage.setFrom(new InternetAddress(env.getProperty("email.username")));
+            helper.setTo(email);
+            helper.setText(message, true);
+//            helper.addAttachment("attachment", new MultipartFile() {
+//            });
+        }catch (MessagingException e){
+            e.printStackTrace();
+        }
 
     }
 }
